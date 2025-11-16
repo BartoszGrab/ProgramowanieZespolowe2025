b67f698b1c28cd67f16e3831b1ed53047faa367b
barteksega1 <barto24.sg@gmail.com>
2025-11-16 16:28:00 +0100

    feat(register) basic register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './components/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+  <BrowserRouter>
     <>
-      <div>
+     <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+    </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,6 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,1 +1,14 @@
 // register
+import React from 'react';
+
+const Register: React.FC = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <p>To jest strona rejestracji</p>
+        </div>
+    );
+};
+
+
+export default Register;
